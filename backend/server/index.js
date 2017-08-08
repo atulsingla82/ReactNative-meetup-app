3a2598c294e50f412c5c82bca9e4c0cae5556e47
atulsingla82 <Lucky@Atuls-MacBook-Pro.local>
2017-08-08 12:22:43 -0400

    "Adding backend server"

---
+++ backend/server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err=> {
+
+if (err){
+
+	console.error(err);
+} {
+	console.log(`App listening on port: ${PORT}`);
+}
+})
