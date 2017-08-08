833fc15a7f60dce01c19f491465316389f4f9ce8
atulsingla82 <Lucky@Atuls-MacBook-Pro.local>
2017-08-08 18:52:50 -0400

    further backend implimentation

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import dbconfig from './config/db';
+import middlewareConfig from'./config/middleware';
+import {MeetupRoutes} from './modules';
 
+// Database
 dbconfig();
 
 const app = express();
 
+
+// Middleware
+middlewareConfig(app);
+
+app.use('/api', [MeetupRoutes]);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, err=> {
---
+++ backend/server/modules/meetups/model.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from 'mongoose';
+
+const MeetupSchema = new Schema ({
+  
+title:{
+	type:String,
+	required:true
+},
+
+description:{
+    type:String,
+	required:true
+
+} 
+
+
+});
+
+export default mongoose.model('Meetup',MeetupSchema);
