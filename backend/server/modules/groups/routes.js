dde897d26f68c738a24a722ae37c781685b64d34
atulsingla82 <asingla@wgu.edu>
2017-08-11 13:29:46 -0400

    Added route

--- backend/server/modules/groups/routes.js
+++ backend/server/modules/groups/routes.js
@@ -4,7 +4,7 @@ import * as GroupController from './controller';
 const routes = new Router();
 
 routes.post('/groups/new', GroupController.createGroup);
-routes.post('/groups/:groupId/meetups/new', GroupController.createGroup);
+routes.post('/groups/:groupId/meetups/new', GroupController.createGroupMeetup);
 
 export default routes;
 
