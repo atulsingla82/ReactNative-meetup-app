0c1eb50a4330d3e0e7c2d07a2c49fd50a2ef4b55
atulsingla82 <asingla@wgu.edu>
2017-08-15 20:01:32 -0400

    backend get route working

--- backend/server/modules/groups/index.js
+++ backend/server/modules/groups/index.js
@@ -1,3 +1,4 @@
 import GroupRoutes from './routes';
 
+
 export { GroupRoutes};
--- backend/server/modules/groups/routes.js
+++ backend/server/modules/groups/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 
 routes.post('/groups/new', GroupController.createGroup);
 routes.post('/groups/:groupId/meetups/new', GroupController.createGroupMeetup);
-routes.get('/groups/:groupId/meetups', GroupController.getGroupMeetup);
+routes.get('/groups/:groupId/meetups', GroupController.getGroupMeetups);
 
 
 
